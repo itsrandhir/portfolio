4c80a4f3227a58061353fc32b5d2fa2dfa99ef8b
itsrandhir <randhir.kumar0604@gmail.com>
2022-09-06 23:59:54 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import * as THREE from "three";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  top:any;
+  left:any;
+  expand = false;
+
+  constructor() {}
+
+  @HostListener('document:click', ['$event'])
+  onClick($event:any){
+    this.expand=true;
+    setTimeout(() =>{
+      this.expand=false;
+    },500)
+  }
+
+  @HostListener('document:mousemove',['$event'])
+  onMousemove($event:any){
+    this.top=($event.pageY-10)+"px";
+    this.left=($event.pageX-10)+"px";
+  }
+  
 }
---
+++ src/app/cube/cube.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, AfterViewInit, ElementRef, Input, ViewChild } from '@angular/core';
+import * as THREE from "three";
+
+@Component({
+  selector: 'app-cube',
+  templateUrl: './cube.component.html',
+  styleUrls: ['./cube.component.css']
+})
+export class CubeComponent implements OnInit {
+  imgSrc:any;
+  
+    constructor() {}
+   
+
+   ngOnInit(): void {
+   }
+  
+  
+ 
+
+}
